8c71bfe6383b1cf0338069c33d7c7866acb4382f
FunctionFn <dfernow@ymail.com>
2015-01-24 15:04:07 -0600

    Pathfinding Changes

--- Grandma/Assets/Scripts/Controllers/BasicPath.cs
+++ Grandma/Assets/Scripts/Controllers/BasicPath.cs
@@ -10,9 +10,16 @@ public class BasicPath : MonoBehaviour {
 	public GameObject garden;
 	public GameObject hospital;
 	public GameObject kitchen;
+	public GameObject clinic;
+	public GameObject rec;
 
 	public int behaviorState;
 	public float travelTime;
+
+	public Vector3 neutralLoc;
+	public Vector3[] pathbn;
+
+	public float waitTime;
 	/*
 	 * Behavior States:
 	 * 0: Neutral
@@ -24,40 +31,48 @@ public class BasicPath : MonoBehaviour {
 	 */
 
 	// Use this for initialization
-	void Start () {
 	
-		behaviorState = 0;
+	void Start () {
 
+		pathbn = new [] { bedroom.transform.position, neutralLoc};
+
+
+
+		behaviorState = 0;
+		beginMovement ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (behaviorState == 0) {
-			changeState ();
-				}
 
-		if (behaviorState != 0 && behaviorState != 5) {
-			beginMovement();
-				}
 		
 	}
 
 	void changeState()
 	{
-		behaviorState = Random.Range(0,5);
+		Debug.Log ("changeState()");
+		behaviorState = Random.Range(0,6);
 	}
 
 	void beginMovement()
 	{
+		Debug.Log ("beginMovement()");
+		behaviorState = 5;
 
+		iTween.MoveTo(gameObject, iTween.Hash ("path", getPath(),
+		                                       "easetype", iTween.EaseType.linear,
+		                                       "time", travelTime,
+		                                       "delay", waitTime,
+		                                       "oncomplete", "beginMovement",
+		                                       "oncompletetarget", gameObject));
+
+		/*
 		if (behaviorState == 1)
 		{
-			iTween.MoveTo(gameObject, iTween.Hash("position", kitchen.transform.position,
-			                         "time", travelTime,
-			                         "easetype", iTween.EaseType.easeInOutCubic,
-			                         "oncomplete", "changeState",
-			                         "oncompletetarget", gameObject));
+			iTween.MoveTo(gameObject, iTween.Hash ("path", bedroom.transform.position,
+			                                       "easetype", iTween.EaseType.linear,
+			                                       "time", travelTime));
 		}
 		if (behaviorState == 2)
 		{
@@ -86,12 +101,53 @@ public class BasicPath : MonoBehaviour {
 			                         "oncompletetarget", gameObject));
 		}
 		
-
+		*/
 
 
 
 	}
 
+	Vector3[] getPath()
+	{
+		changeState ();
+		Debug.Log ("getPath()");
+		Vector3[] path = new [] { 
+			gameObject.transform.position, 
+			neutralLoc, 
+			getDestination().transform.position };
+		return path;
+	}
+	
+	GameObject getDestination(){
+
+		Debug.Log ("getDestination()");
+
+		if (behaviorState == 1)
+		{
+			return kitchen;
+		}
+		if (behaviorState == 2)
+		{
+			return hospital;
+		}
+		if (behaviorState == 3)
+		{
+			return garden;
+		}
+		if (behaviorState == 4) {
+			return clinic;
+
+				}
+		if (behaviorState == 5)
+		{
+			return rec;
+		}
+		else
+		{
+			return bedroom;
+		}
+
+	}
 
 }
 
---
+++ Grandma/Assets/Scripts/Controllers/iTweenPathTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iTweenPathTest : MonoBehaviour {
+
+
+	public GameObject bedroom;
+	public GameObject garden;
+	public GameObject hospital;
+	public GameObject kitchen;
+
+	public float moveTime;
+	
+	public Vector3 neutralLoc;
+
+	public Vector3[] pathbg; 
+
+	// Use this for initialization
+	void Start () {
+		pathbg = new [] { bedroom.transform.position, neutralLoc, garden.transform.position};
+		iTween.PutOnPath (gameObject, pathbg, 0.3f);
+
+		followPath();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+		}
+	void followPath()
+	{
+		Debug.Log ("MoveTo");
+		iTween.MoveTo(gameObject, iTween.Hash ("path", pathbg,
+		                                       "easetype", iTween.EaseType.linear,
+		                                       "time", moveTime));
+
+	}
+}
