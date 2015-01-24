b2d3e5064c5421af028b287191b7b6dcb78edbe5
unknown <aarmst10@532LAB07.cstcis.cti.depaul.edu>
2015-01-24 13:30:20 -0600

    These are a new repo on my machine what is git?

---
+++ GrandmasCode/GrandmasCode/_State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandmasCode
+{
+    class _State
+    {
+        public void enter()
+        {
+        }
+
+        public void execute()
+        {
+        }
+
+        public void exit()
+        {
+        }
+
+
+    }
+}
