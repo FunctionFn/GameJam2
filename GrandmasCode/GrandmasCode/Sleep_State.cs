327260a91202bbb355f800e5e989b23034ce8dcc
unknown <VFERNAN5@532LAB09.cstcis.cti.depaul.edu>
2015-01-24 15:56:52 -0600

    The Sleep state

---
+++ GrandmasCode/GrandmasCode/Sleep_State.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandmasCode
+{
+    class Sleep_State : _State
+    {
+        private static _State instance;
+
+        public _State getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Sleep_State();
+            }
+            return instance;
+        }
+
+        private Sleep_State()
+        {
+            // Do nothing
+        }
+
+        void enter(NPC character)
+        {
+            Console.WriteLine(character.name + " enters Sleep state");
+        }
+
+        void execute(NPC character)
+        {
+            Console.WriteLine(character.name + " executes Sleep state");
+            if (World.isNight())
+            {
+                //character.ChangeState(WDWDN_State.getInstance());
+            }
+        }
+
+        void exit(NPC character)
+        {
+            Console.WriteLine(character.name + " exits Sleep state");
+        }
+    }
+}
