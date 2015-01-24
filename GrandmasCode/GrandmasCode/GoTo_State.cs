49f454e1070b495b16a6598bca93baa6d9f24ce9
unknown <aarmst10@532LAB07.cstcis.cti.depaul.edu>
2015-01-24 15:48:35 -0600

    added gotokitchen state

--- GrandmasCode/GrandmasCode/GoTo_State.cs
+++ GrandmasCode/GrandmasCode/GoTo_State.cs
@@ -6,22 +6,40 @@ using System.Threading.Tasks;
 
 namespace GrandmasCode
 {
-    class GoTo_State: _State
+    class GoToKitchen_State: _State
     {
+        private GoToKitchen_State()
+        {
+        }
+
+        public _State getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new GoToKitchen_State();
+            }
+
+            return instance;
+        }
+
         void _State.enter(NPC character)
         {
-            Console.Write("entering goto state");
+            Console.Write(character.name + "entering goto state");
         }
 
         void _State.execute(NPC chararcter)
         {
-
+            
         }
 
         void _State.exit(NPC character)
         {
-            Console.Write("exiting goto state");
+            Console.Write(character.name + "exiting goto state");
         }
+
+        //-------------------------------------------
+        private static _State instance;
+
 
     }
 }
