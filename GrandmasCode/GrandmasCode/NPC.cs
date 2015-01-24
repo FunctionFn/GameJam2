ceafa8ac5a89d5c6d895859230d5a32e24c75d4f
unknown <aarmst10@532LAB07.cstcis.cti.depaul.edu>
2015-01-24 13:12:24 -0600

    Commit and push; the _State class and NPC class added

---
+++ GrandmasCode/GrandmasCode/NPC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GrandmasCode
+{
+
+    public enum Constants
+    {
+        STARTING_VIT = 70
+    }
+
+    class NPC
+    {
+
+        public NPC(string inName)
+        {
+            this.name = inName;
+            this.vit = (int)Constants.STARTING_VIT;
+            this.curr_state = null;
+            //this.thresholds = null;
+        }
+
+
+        public void Update()
+        {
+
+        }
+
+
+        public void ChangeState(_State newState)
+        {
+            this.curr_state = newState;
+        }
+
+        //----------------
+        public string name;
+        public _State curr_state;
+        // public Threshold tresholds;
+        public int vit;
+
+
+    }
+}
