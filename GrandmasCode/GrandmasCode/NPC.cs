1f4c2ee3b57f559c547308954cd4c49e68433dc5
unknown <VFERNAN5@532LAB09.cstcis.cti.depaul.edu>
2015-01-25 02:17:54 -0600

    Framework for phrases.

--- GrandmasCode/GrandmasCode/NPC.cs
+++ GrandmasCode/GrandmasCode/NPC.cs
@@ -14,6 +14,11 @@
     public void Update()
     {
         this.curr_state.execute(this);
+
+        if (World.getInstance().getRandom() < Constants.SAY_SOMETHING_CHANCE)
+        {
+            this.isTalking = true;
+        }
     }
 
 
@@ -22,7 +27,70 @@
         this.curr_state.exit(this);
         this.curr_state = newState;
         this.curr_state.enter(this);
-
+        if (this.curr_state == Decision_State.getInstance())
+        {
+            this.curr_phrase = this.sayDecision;
+        }
+        else if (this.curr_state == Sleep_State.getInstance())
+        {
+            this.curr_phrase = this.saySleep;
+        }
+        else if (this.curr_state == GoToSleep_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToSleep;
+        }
+        else if (this.curr_state == Kitchen_State.getInstance())
+        {
+            this.curr_phrase = this.sayKitchen;
+        }
+        else if (this.curr_state == GoToKitchen_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToKitchen;
+        }
+        else if (this.curr_state == Den_State.getInstance())
+        {
+            this.curr_phrase = this.sayDen;
+        }
+        else if (this.curr_state == GoToDen_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToDen;
+        }
+        else if (this.curr_state == Garden_State.getInstance())
+        {
+            this.curr_phrase = this.sayGarden;
+        }
+        else if (this.curr_state == GoToGarden_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToGarden;
+        }
+        else if (this.curr_state == PT_State.getInstance())
+        {
+            this.curr_phrase = this.sayPT;
+        }
+        else if (this.curr_state == GoToPT_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToPT;
+        }
+        else if (this.curr_state == Reminisce_State.getInstance())
+        {
+            this.curr_phrase = this.sayReminisce;
+        }
+        else if (this.curr_state == Nap_State.getInstance())
+        {
+            this.curr_phrase = this.sayNap;
+        }
+        else if (this.curr_state == Wander_State.getInstance())
+        {
+            this.curr_phrase = this.sayWander;
+        }
+        else if (this.curr_state == Infirmary_State.getInstance())
+        {
+            this.curr_phrase = this.sayInfirmary;
+        }
+        else if (this.curr_state == GoToInfirmary_State.getInstance())
+        {
+            this.curr_phrase = this.sayGoToInfirmary;
+        }
     }
 
     public bool ChangeRoom(Room newRoom)
@@ -43,6 +111,26 @@
     public Threshold thresholds;
     public int vit;
     public bool isWalking;
+    public bool isTalking;
+    public string curr_phrase;
+
+    public string sayDecision;
+    public string sayKitchen;
+    public string sayGoToKitchen;
+    public string sayPT;
+    public string sayGoToPT;
+    public string sayGarden;
+    public string sayGoToGarden;
+    public string sayDen;
+    public string sayGoToDen;
+    public string sayInfirmary;
+    public string sayGoToInfirmary;
+    public string saySleep;
+    public string sayGoToSleep;
+    public string sayReminisce;
+    public string sayNap;
+    public string sayWander;
+
 
 }
 
